a41aabd4af758bf09ef307709521083ecd57c23c
Pedro Henrique <pedrorjas@gmail.com>
2015-06-07 18:45:44 -0300

    Adicionado variáveis e declarado do (TextView) na main.

--- app/src/main/java/com/example/pedro/cnc_calc/Tabela_rosca.java
+++ app/src/main/java/com/example/pedro/cnc_calc/Tabela_rosca.java
@@ -1,22 +1,54 @@
 package com.example.pedro.cnc_calc;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
 public class Tabela_rosca extends ActionBarActivity {
 
-    String[] M = new String[]{"1","2", "3"};
+    EditText et_M;
+    TextView passoFina_M, passoGrossa_M, externo_fina_M, externo_grossa_M;
+    TextView passoFina_W, passoGrossa_W, externo_fina_W, externo_grossa_W;
+    Double P;
+    Double M;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tabela_rosca);
 
+        /** Inicia as variaveis textView (Exibição valores Whithworth) */
+        passoFina_W = (TextView) findViewById(R.id.tv_fina_Ww);
+        passoGrossa_W = (TextView) findViewById(R.id.tv_grossa_Ww);
+        externo_fina_W = (TextView) findViewById(R.id.tv_externo_fina_Ww);
+        externo_grossa_W = (TextView) findViewById(R.id.tv_externo_grossa_Ww);
 
+        /** Inicia as variaveis textView (Exibição valores Métricos) */
+        passoFina_M = (TextView) findViewById(R.id.tv_fina_M);
+        passoGrossa_M = (TextView) findViewById(R.id.tv_grossa_M);
+        externo_fina_M = (TextView) findViewById(R.id.tv_externo_fina_M);
+        externo_grossa_M = (TextView) findViewById(R.id.tv_externo_grossa_M);
 
+        et_M = (EditText) findViewById(R.id.et_M);
+        M = Double.parseDouble(et_M.getText().toString());
+
+        if (et_M != null){
+            formula_M(M);
+            passoFina_M.setText(M.toString());
+        }
+
+    }
+
+    private Double formula_M(double m){
+        return m;
+    }
+
+    private Double formula_Ww(double m){
+        return m;
     }
 
     @Override
